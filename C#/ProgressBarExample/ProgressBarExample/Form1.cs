d3a2d72e63371c214e90ebe07798080ae5f03692
adamelders <Ramor.1990@gmail.com>
2016-03-12 06:50:16 -0500

    Added  a simple BackgroundWorker example. This wasn't a school project, but something I needed for a program I am currently developing.

---
+++ C#/ProgressBarExample/ProgressBarExample/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace ProgressBarExample
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                Thread.Sleep(100);
+                backgroundWorker1.ReportProgress(i);
+            }
+        }
+
+        private void backgroundWorker1_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            progressBar1.Value = e.ProgressPercentage;
+            this.Text = e.ProgressPercentage.ToString();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!backgroundWorker1.IsBusy)
+            {
+                label1.Text = "Running...";
+                backgroundWorker1.RunWorkerAsync();
+            }
+            else
+                MessageBox.Show("Can't run the process twice, please wait!");
+        }
+
+        private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            label1.Text = "Completed!";
+        }
+    }
+}
